03209cf4b4cd61a9d53970197c8c6b4756eee935
AlejandroAlv12 <00211623@uca.edu.sv>
2026-06-23 03:32:18 -0600

    feat: cambiar color

--- app/src/main/java/com/pdm0126/outfix/ui/theme/Color.kt
+++ app/src/main/java/com/pdm0126/outfix/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val LimeGreen = Color(0xFF8F5A5A)
+val LimeGreen = Color(0xFF00A31B)
 val GlassWhite = Color(0x33FFFFFF)
 val DarkBackground = Color(0xFF121212)
 val DarkSurface = Color(0xFF1E1E1E)
